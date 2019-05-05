c4ded3b473deed9d1f3529a4ef6d317519e6b0fb
Asharia Nurse <gic_uwi@GIC-UWIdeiMac.local>
2019-05-05 20:29:04 +0800

    A major undertaking had been had

--- FileExplorer/SecondscreenTableViewController.swift
+++ FileExplorer/SecondscreenTableViewController.swift
@@ -16,34 +16,59 @@ struct FolderInfo: Decodable{
     
 }
 
+var files = [""]
+var folders = [""]
+var current = ""
+var parent = ""
+
+let delay = DispatchGroup()
+
 
 class SecondscreenTableViewController: UITableViewController {
     //var folder = FolderInfo.self
+//    var folder: FolderInfo?
     
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = "http://127.0.0.1:8000"
-        let urlObj = URL(string: url)
-        
-        URLSession.shared.dataTask(with: urlObj!) {(data,response,error) in
-            do{
-                var Folders = try JSONDecoder().decode([FolderInfo].self, from:data!)
-                
-              //for folder in Folders{
-                   //print(folder.folders)
-//                }
-                //let folders = Folders["folders"]
-                
-                
-            }catch{
-                let alert = UIAlertController(title: "Oops", message: "There was an error while parsing the data", preferredStyle: UIAlertController.Style.alert)
-                alert.addAction(UIAlertAction(title:"OK", style: UIAlertAction.Style.default, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-                
-            }
-            
-        }.resume()
+//        let url = "http://127.0.0.1:8000/?folder=/"
+//        let urlObj = URL(string: url)
+//        
+//        print("Table")
+//        
+////        delay.enter()
+//        
+//        URLSession.shared.dataTask(with: urlObj!) {(data,response,error) in
+//            do{
+//                let Folders = try JSONDecoder().decode(FolderInfo.self, from:data!)
+//                
+//                files = Folders.files
+//                folders = Folders.folders
+//                current = Folders.current
+////                parent = Folders.parent
+//                
+//                delay.leave()
+//                
+//                
+//                
+//                
+//                //let folder = Folders["folder"]
+//              //for folder in Folders{
+//                   //print(folder.folders)
+////                }
+//                //let folders = Folders["folders"]
+//                
+//                
+//            }catch{
+//                let alert = UIAlertController(title: "Oops", message: "There was an error while parsing the data", preferredStyle: UIAlertController.Style.alert)
+//                alert.addAction(UIAlertAction(title:"OK", style: UIAlertAction.Style.default, handler: nil))
+//                self.present(alert, animated: true, completion: nil)
+//                
+//            }
+//            
+//        }.resume()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -53,32 +78,61 @@ class SecondscreenTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
+    //override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+     //   return folder[FolderInfo]
+   // }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        //to display the number of rows as is items in folders
-        //guard let folder = self.folder else {
-        
-            return 0//folder.FolderInfo.count
-        
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        //to display the number of rows as is items in folders
+//        guard let folder = self.folder else {
+//            return 0
+//        }
+//        //max(folder.folders.count, folder.files)
+//        //return   //folder.folders.count
+//        return 4
+//        
+//        
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        
-        return 0
+        print(folders.count + files.count)
+        print(folders)
+        print(files)
+            return folders.count + files.count
     }
 
     
-    private static let cellReuseIdentifier = "Identifier"
+//    private static let cellReuseIdentifier = "Identifier"
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: SecondscreenTableViewController.cellReuseIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Identifier", for: indexPath)
         
-        if cell == nil{
-            cell = UITableViewCell.init(style: .default, reuseIdentifier: SecondscreenTableViewController.cellReuseIdentifier)
-
-       }
+        cell.backgroundColor = UIColor.red
+        
+        var Displaythings = [""]
+        
+        Displaythings = folders + files
+        
+        cell.textLabel?.text = Displaythings[indexPath.row]
+        
+        //cell.textLabel?.text = folders[indexPath.row]
+        //cell.textLabel?.text = current[indexPath.row]
+        
+        
+//        if cell == nil{
+//            cell = UITableViewCell.init(style: .default, reuseIdentifier: SecondscreenTableViewController.cellReuseIdentifier)
+//
+//       }
+        
+       // let listitems = FolderInfo[indexPath.row]
+        //cell.textLabel?.text = self.folder?.current
+        
+        //let folderitem1 = self.folder?.files
+        //let folderitem2 = self.folder?.folders
+        
+        //cell.textLabel?.text = self.folder?.files
         
         //cell.textLabel?.text = indexPath.description
         //let folderitems = FolderInfo[indexPath.row]
--- FileExplorer/ViewController.swift
+++ FileExplorer/ViewController.swift
@@ -13,11 +13,49 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        delay.enter()
         
+        let url = "http://127.0.0.1:8000/?folder=/"
+        let urlObj = URL(string: url)
+        
+        print("Table")
+        
+        //        delay.enter()
+        
+        URLSession.shared.dataTask(with: urlObj!) {(data,response,error) in
+            do{
+                let Folders = try JSONDecoder().decode(FolderInfo.self, from:data!)
+                
+                files = Folders.files
+                folders = Folders.folders
+                current = Folders.current
+                //                parent = Folders.parent
+                
+                delay.leave()
+                
+                
+                
+                
+                //let folder = Folders["folder"]
+                //for folder in Folders{
+                //print(folder.folders)
+                //                }
+                //let folders = Folders["folders"]
+                
+                
+            }catch{
+                let alert = UIAlertController(title: "Oops", message: "There was an error while parsing the data", preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title:"OK", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                
+            }
+            
+            }.resume()
     }
     @IBOutlet weak var openTableView: UIButton?
     @IBAction func openTableView(_sender:UIButton){
         //this button will open the the table view
+        delay.wait()
         performSegue(withIdentifier: "tothetable", sender: self)
     }
 
